390a7ea3e2a9050e81630a18d5119e913324790e
SV. NAVANEET <connectsvn@gmail.com>
2026-09-18 14:44:43 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
 <?php
-
-$conn = new mysqli("localhost", "root", "Navaneet@3", "registration_system", 3307);
+$conn =new mysqli("localhost", "root", "Navaneet@3", "registration_system", 3307);
 $manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017");
-
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
